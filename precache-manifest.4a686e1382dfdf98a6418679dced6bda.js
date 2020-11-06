d7a8df7694d2d424ee5d2d768ee2df25ac667646
quilaton <quilaton@justin.tv>
2020-11-06 15:51:41 -0800

    Static site set up

---
+++ precache-manifest.4a686e1382dfdf98a6418679dced6bda.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0f93cdfa90afa85570ee30f64923b15f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a80c5484c6b74f394b7b",
+    "url": "/static/css/2.a7049421.chunk.css"
+  },
+  {
+    "revision": "96c706dda962c93eb5bb",
+    "url": "/static/css/main.aa500963.chunk.css"
+  },
+  {
+    "revision": "a80c5484c6b74f394b7b",
+    "url": "/static/js/2.ee100f94.chunk.js"
+  },
+  {
+    "revision": "1bd38dfc7f3d19772812daabb914b1ec",
+    "url": "/static/js/2.ee100f94.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "96c706dda962c93eb5bb",
+    "url": "/static/js/main.046085a5.chunk.js"
+  },
+  {
+    "revision": "be83d69e4aa0b3159843",
+    "url": "/static/js/runtime-main.d356af40.js"
+  },
+  {
+    "revision": "7fda562c82c09fbabec229b0b7ebb01c",
+    "url": "/static/media/Roobert-Bold.7fda562c.woff2"
+  },
+  {
+    "revision": "e1ed2e0bbb575f1a395b398460f55e70",
+    "url": "/static/media/Roobert-Bold.e1ed2e0b.woff"
+  },
+  {
+    "revision": "014ea096b0f865fa4dd94f1510fc8762",
+    "url": "/static/media/Roobert-Regular.014ea096.woff"
+  },
+  {
+    "revision": "3fe213a3618624fd7b716f8d52dff334",
+    "url": "/static/media/Roobert-Regular.3fe213a3.woff2"
+  },
+  {
+    "revision": "48a406bf82c48cfaa849f73f7c30df48",
+    "url": "/static/media/Roobert-SemiBold.48a406bf.woff"
+  },
+  {
+    "revision": "df85158a0012c224b0216b7a2b287cdc",
+    "url": "/static/media/Roobert-SemiBold.df85158a.woff2"
+  }
+]);
